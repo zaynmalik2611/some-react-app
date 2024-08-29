2a9091808b2dd1c23c7d35332316381d5432f495
zaynmalikikbits <zainul.abideen@kikbits.com>
2024-08-30 03:05:54 +0500

    initialized git repo and setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+          <h1 className="text-3xl font-bold ">Some react app</h1>
+        </div>
+        <div className=" flex justify-center">
+          <div className="border p-4 mt-6 rounded-lg">
+            Some react app. I dunno what to do about it.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
